5db5831f3262d7d9a66d65c21d40d0d791226d9d
mooseheaddigital19 <marianna@mooseheaddigital.com>
2025-11-26 13:16:33 -0500

    Update nearest-store.js

--- netlify/functions/nearest-store.js
+++ netlify/functions/nearest-store.js
@@ -297,7 +297,7 @@ exports.handler = async (event) => {
 
     const html = `
       <div style="font-family: Arial, sans-serif; font-size:14px; line-height:1.4;">
-        <p style="font-family: Arial, sans-serif; font-size:16px; line-height:1.4;color: rgb(66, 85, 99);font-size:18px;"><strong>${headingText}</strong></p>
+        //<p style="font-family: Arial, sans-serif; font-size:16px; line-height:1.4;color: rgb(66, 85, 99);font-size:18px;"><strong>${headingText}</strong></p>
         ${storesHtml}
       </div>
     `;
